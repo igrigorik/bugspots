716b9d8095f3ec92189051988ce2f5eb34fa37dd
Ilya Grigorik <ilya@igvita.com>
2011-12-14 22:53:37 -0800

    refactor + a few fixes

--- lib/bugspots/scanner.rb
+++ lib/bugspots/scanner.rb
@@ -3,16 +3,16 @@ require 'grit'
 
 module Bugspots
   Fix = Struct.new(:message, :date, :files)
+  Spot = Struct.new(:file, :score)
 
-  def self.scan(repo)
-    puts "Scanning #{repo} repo".foreground(:green)
+  def self.scan(repo, depth = 500)
     repo = Grit::Repo.new(repo)
     fixes = []
 
-    repo.commits('master', 500).each do |commit|  
+    repo.commits('master', depth).each do |commit|  
       if commit.message =~ /fix(es|ed)|close(s|d)/
         files = commit.stats.files.map {|s| s.first}    
-        fixes << Fix.new(commit.message, commit.date, files)        
+        fixes << Fix.new(commit.short_message, commit.date, files)
       end
     end
 
@@ -20,15 +20,14 @@ module Bugspots
     fixes.each do |fix|
       fix.files.each do |file|
         t = 1 - ((Time.now - fix.date).to_f / (Time.now - fixes.last.date))
-        hotspots[file] += (1/(1+Math.exp(-12*t)+12))
+        hotspots[file] += 1/(1+Math.exp((-12*t)+12))
       end
     end
 
-    puts "\tFound #{fixes.size} bugfix commits, with #{hotspots.size} hotspots:".foreground(:yellow)
-    puts
-
-    hotspots.sort_by {|k,v| v}.reverse.each do |spot|
-      puts "\t#{spot.last.round(2)}".foreground(:red) + " - #{spot.first}".foreground(:yellow)
+    spots = hotspots.sort_by {|k,v| v}.reverse.collect do |spot|
+      Spot.new(spot.first, spot.last.round(4))
     end
+
+    return fixes, spots
   end
 end
